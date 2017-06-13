1baa169919af9625a37beac72a46ca117d59e413
huajianjiu <lastranger8811@gmail.com>
2017-06-14 03:24:02 +0900

    start to build val task model

--- cmwe.py
+++ cmwe.py
@@ -9,7 +9,9 @@ from __future__ import print_function
 
 import numpy as np
 import tensorflow as tf
-import os
+import os, re
+import pandas as pd
+from bs4 import BeautifulSoup
 
 from keras.preprocessing.text import Tokenizer
 from keras.preprocessing.sequence import pad_sequences
@@ -23,6 +25,10 @@ from keras.models import Model
 from mulembedding import MulEmbedding
 from attention import AttentionWithContext
 from embeddingdot import EmbeddingDot
+
+from keras.preprocessing.text import Tokenizer, text_to_word_sequence
+from keras.preprocessing.sequence import pad_sequences
+from keras.utils.np_utils import to_categorical
 
 flags = tf.app.flags
 
@@ -65,8 +71,6 @@ flags.DEFINE_float("subsample", 1e-4,
 flags.DEFINE_integer("context_size", 10,
                      "The context, size. If you change it, you"
                      "Need to also change it in embeddings/word2vec_kernels.cc now.")
-flags.DEFINE_integer("rnn_time_steps", 10,
-                     "The time steps for the rnn.")
 
 FLAGS = flags.FLAGS
 
@@ -90,7 +94,6 @@ class Options(object):
         self.prototypes = FLAGS.prototypes
         self.pretrained_emb = FLAGS.pretrained_emb
         self.context_size = FLAGS.context_size
-        self.time_steps = FLAGS.rnn_time_steps
 
 
 def data_reader_ts(opts):
@@ -111,11 +114,11 @@ def data_reader_ts(opts):
             labels, contexts)
 
 
-def build_final_embedding(opts, word_input, context_input):
+def build_final_embedding(opts, word_inputs, context_inputs):
     vocab = opts.vocab
     # create single prototype embedding layer
     if opts.pretrained_emb is None:
-        single_embedding_layer = Embedding(input_dim=len(vocab) + 1,
+        single_embedding_layer = Embedding(input_dim=opts.vocab_size,
                                            output_dim=opts.emb_dim,
                                            input_length=opts.context_size,
                                            trainable=True)
@@ -138,29 +141,27 @@ def build_final_embedding(opts, word_input, context_input):
                 # words not found in embedding index will be all-zeros.
                 single_embedding_matrix[i] = embedding_vector
 
-        single_embedding_layer = Embedding(len(vocab) + 1,
+        single_embedding_layer = Embedding(opts.vocab_size,
                                            opts.emb_dim,
                                            weights=[single_embedding_matrix],
                                            input_length=opts.context_size,
-                                           trainable=False)
+                                           trainable=True)
 
     # create multiple prototype embedding layer
-    multiple_embedding_layer = MulEmbedding(input_dim=len(vocab) + 1,
+    multiple_embedding_layer = MulEmbedding(input_dim=opts.vocab_size,
                                             output_prototypes=opts.prototypes,
                                             output_dim=opts.emb_dim,
-                                            input_length=None,
                                             is_word_input=True,
                                             trainable=True)
 
     # [batch_size, prototypes, emb_dim]
-    multiple_embeded = multiple_embedding_layer(word_input)
-
-    embedded_context = single_embedding_layer(context_input)
-    l_lstm = Bidirectional(GRU(opts.time_steps, return_sequences=True))(embedded_context)
+    multiple_embeded = multiple_embedding_layer(word_inputs)
+    embedded_context = single_embedding_layer(context_inputs)
+    l_gru = Bidirectional(GRU(opts.emb_dim, return_sequences=True))(embedded_context)
     # [batch_size, prototypes]
-    l_dense = TimeDistributed(Dense(opts.prototypes))(l_lstm)
+    l_dense = TimeDistributed(Dense(opts.prototypes))(l_gru)
     l_att = AttentionWithContext()(l_dense)
-
+    #
     # get final embedding
     embedding_dot = EmbeddingDot(axes=[1, 1])
     final_embedding = embedding_dot([multiple_embeded, l_att])
@@ -168,7 +169,74 @@ def build_final_embedding(opts, word_input, context_input):
     return final_embedding
 
 
-def main():
+def build_nce_w2v_forward(opts, embedding, word_inputs, label_inputs):
+    """
+    Build the forword nn to train word embeddings
+    :param opts: Option instantce
+    :param embedding: Output tensor of Final embedding layer
+    :param word_inputs: placeholder or Input layer for word inputs
+    :param label_inputs: placeholder or Inpu layer for label inputs
+    :return: true_logits, sampled_logits
+    """
+    # Softmax weight: [vocab_size, emb_dim]. Transposed.
+    sm_w_t = tf.Variable(
+        tf.zeros([opts.vocab_size, opts.emb_dim]),
+        name="sm_w_t")
+
+    # Softmax bias: [vocab_size].
+    sm_b = tf.Variable(tf.zeros([opts.vocab_size]), name="sm_b")
+
+    # Nodes to compute the nce loss w/ candidate sampling.
+    labels_matrix = tf.reshape(
+        tf.cast(label_inputs,
+                dtype=tf.int64),
+        [opts.batch_size, 1])
+
+    # Negative sampling.
+    sampled_ids, _, _ = (tf.nn.fixed_unigram_candidate_sampler(
+        true_classes=labels_matrix,
+        num_true=1,
+        num_sampled=opts.num_samples,
+        unique=True,
+        range_max=opts.vocab_size,
+        distortion=0.75,
+        unigrams=opts.vocab_counts.tolist()))
+
+    # Weights for labels: [batch_size, emb_dim]
+    true_w = tf.nn.embedding_lookup(sm_w_t, label_inputs)
+    # Biases for labels: [batch_size, 1]
+    true_b = tf.nn.embedding_lookup(sm_b, label_inputs)
+
+    # Weights for sampled ids: [num_sampled, emb_dim]
+    sampled_w = tf.nn.embedding_lookup(sm_w_t, sampled_ids)
+    # Biases for sampled ids: [num_sampled, 1]
+    sampled_b = tf.nn.embedding_lookup(sm_b, sampled_ids)
+
+    # True logits: [batch_size, 1]
+    true_logits = tf.reduce_sum(tf.multiply(embedding, true_w), 1) + true_b
+
+    # Sampled logits: [batch_size, num_sampled]
+    # We replicate sampled noise labels for all examples in the batch
+    # using the matmul.
+    sampled_b_vec = tf.reshape(sampled_b, [opts.num_samples])
+    sampled_logits = tf.matmul(embedding,
+                               sampled_w,
+                               transpose_b=True) + sampled_b_vec
+    return true_logits, sampled_logits
+
+
+def train_language_model(opts):
+    # Global step: scalar, i.e., shape [].
+    global_step = tf.Variable(0, name="global_step")
+    # TODO
+
+
+def build_HATT_RNN(opts, embedding, sentence_input, review_input):
+    # TODO build HATT model
+    pass
+
+
+def word_embedding_task():
     opts = Options()
     session = tf.Session()
 
@@ -177,6 +245,7 @@ def main():
 
     (opts.vocab) = session.run([words])
 
+    opts.vocab_size = len(opts.vocab)
     _id2word = vocab = opts.vocab
     _word2id = {}
     for i, w in enumerate(_id2word):
@@ -184,12 +253,12 @@ def main():
 
     # batch word input
     # [batch_size, ]
-    word_input = Input(shape=(1,), dtype='int32')
+    word_inputs = Input(batch_shape=(opts.batch_size, 1), dtype='int32')
     # batch context input
     # [batch_size, context_size]
-    context_input = Input(shape=(opts.context_size,))
+    context_inputs = Input(batch_shape=(opts.batch_size, opts.context_size))
 
-    final_embedding = build_final_embedding(opts, word_input, context_input)
+    final_embedding = build_final_embedding(opts, word_inputs, context_inputs)
 
     # TODO: Loop in batch and epoch.
     # TODO: Note: a new epoch start only when the data gatherer returns a new epoch number
@@ -201,29 +270,140 @@ def main():
     # TODO: see https://keras.io/getting-started/faq/#how-can-i-obtain-the-output-of-an-intermediate-layer
 
 
-def test_unit():
+def read_text_classification_task_data(opts):
+    # TODO: check if it is ok for fine-grained task
+    MAX_SENT_LENGTH = opts.MAX_SENT_LENGTH
+    MAX_SENTS = opts.MAX_SENTS
+    MAX_NUM_WORDS = opts.MAX_NUM_WORDS # Maximum number of words to work with
+                          # (if set, tokenization will be restricted to the top
+                          # num_words most common words in the dataset).
+    VALIDATION_SPLIT = opts.VALIDATION_SPLIT
+    def clean_str(string):
+        """
+        Tokenization/string cleaning for dataset
+        Every dataset is lower cased except
+        """
+        string = re.sub(r"\\", "", string)
+        string = re.sub(r"\'", "", string)
+        string = re.sub(r"\"", "", string)
+        return string.strip().lower()
+
+    data_train = pd.read_csv('~/Testground/data/imdb/labeledTrainData.tsv', sep='\t')
+    print
+    data_train.shape
+
+    from nltk import tokenize
+
+    reviews = []
+    labels = []
+    texts = []
+
+    for idx in range(data_train.review.shape[0]):
+        text = BeautifulSoup(data_train.review[idx])
+        text = clean_str(text.get_text().encode('ascii', 'ignore'))
+        texts.append(text)
+        sentences = tokenize.sent_tokenize(text)
+        reviews.append(sentences)
+
+        labels.append(data_train.sentiment[idx])
+
+    tokenizer = Tokenizer(nb_words=MAX_NUM_WORDS)
+    tokenizer.fit_on_texts(texts)
+
+    data = np.zeros((len(texts), MAX_SENTS, MAX_SENT_LENGTH), dtype='int32')
+
+    for i, sentences in enumerate(reviews):
+        for j, sent in enumerate(sentences):
+            if j < MAX_SENTS:
+                wordTokens = text_to_word_sequence(sent)
+                k = 0
+                for _, word in enumerate(wordTokens):
+                    if k < MAX_SENT_LENGTH and tokenizer.word_index[word] < MAX_NUM_WORDS:
+                        data[i, j, k] = tokenizer.word_index[word]
+                        k = k + 1
+
+    word_index = tokenizer.word_index
+    print('Total %s unique tokens.' % len(word_index))
+
+    labels = to_categorical(np.asarray(labels))
+    print('Shape of data tensor:', data.shape)
+    print('Shape of label tensor:', labels.shape)
+
+    indices = np.arange(data.shape[0])
+    np.random.shuffle(indices)
+    data = data[indices]
+    labels = labels[indices]
+    nb_validation_samples = int(VALIDATION_SPLIT * data.shape[0])
+
+    x_train = data[:-nb_validation_samples]
+    y_train = labels[:-nb_validation_samples]
+    x_val = data[-nb_validation_samples:]
+    y_val = labels[-nb_validation_samples:]
+
+    print('Number of reviews for training and test')
+    print(y_train.sum(axis=0))
+    print(y_val.sum(axis=0))
+
+    return word_index, x_train, y_train, x_val, y_val
+
+
+def text_classification_task():
+    opts = Options()
+    opts.MAX_SENT_LENGTH = MAX_SENT_LENGTH = 100
+    opts.MAX_SENTS = MAX_SENTS = 15
+    opts.MAX_NUM_WORDS = MAX_NUM_WORDS = 20000 # Maximum number of words to work with
+                          # (if set, tokenization will be restricted to the top
+                          # num_words most common words in the dataset).
+    opts.VALIDATION_SPLIT = VALIDATION_SPLIT = 0.2
+
+    # Read data
+    if opts.pretrained_emb is None:
+        opts.pretrained_emb = "/home/ke/data/glove/glove.6B.100d.txt"
+
+    (word_index, x_train, y_train, x_val, y_val) = \
+        read_text_classification_task_data(opts)
+
+    opts.vocab_size = len(word_index) + 1
+
+    sentence_input = Input(shape=(MAX_SENT_LENGTH,), dtype='int32')
+    review_input = Input(shape=(MAX_SENTS, MAX_SENT_LENGTH), dtype='int32')
+
+    # TODO: test if the two input holder above safely changes to multi-prototype
+    # TODO: build and compile CMWE-HATT model
+    # batch word input
+    # [batch_size, ]
+    word_inputs = Input(batch_shape=(opts.batch_size, 1), dtype='int32')
+    # batch context input
+    # [batch_size, context_size]
+    context_inputs = Input(batch_shape=(opts.batch_size, opts.context_size))
+
+    final_embedding = build_final_embedding(opts, word_inputs, context_inputs)
+
+
+def test_final_embedding_unit():
     opts = Options()
     # For unittest
     opts.vocab = range(25)
+    opts.vocab_size = 25
     input_words = np.random.randint(25, size=(opts.batch_size,))
     input_contexts = np.random.randint(25, size=(opts.batch_size, opts.context_size))
 
     # batch word input
     # [batch_size, ]
-    word_input = Input(shape=(1,), dtype='int32')
+    word_inputs = Input(batch_shape=(opts.batch_size, 1), dtype='int32')
     # batch context input
     # [batch_size, context_size]
-    context_input = Input(shape=(opts.context_size,))
+    context_inputs = Input(batch_shape=(opts.batch_size, opts.context_size))
 
-    final_embedding = build_final_embedding(opts, word_input, context_input)
+    final_embedding = build_final_embedding(opts, word_inputs, context_inputs)
 
-    model = Model(inputs=[word_input, context_input],
+    model = Model(inputs=[word_inputs, context_inputs],
                   outputs=[final_embedding])
     model.compile('rmsprop', 'mse')
     output_array = model.predict([input_words, input_contexts])
-    print(output_array[0].shape)
+    print(output_array.shape)
 
 
 if __name__ == "__main__":
     # main()
-    test_unit()
+    test_final_embedding_unit()
